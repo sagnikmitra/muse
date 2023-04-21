22304c36d0dc0d34131a7fd0bfd10c803ab6b092
Sagnik Mitra <sagnikmitra123@gmail.com>
2023-04-22 00:43:57 +0530

    muse

---
+++ js/music-list.js
@@ -0,0 +1,86 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Labon Ko",
+    artist: "KK",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Saiyaan",
+    artist: "Kailash Kher",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Phir Mohabbat",
+    artist: "Arijit Singh",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Bojheyna Se Bojheyna",
+    artist: "Arijit Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Aaj Jaane Ki Zid",
+    artist: "Farida Khannum",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Jeena Jeena",
+    artist: "Atif Aslam",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Tu Hi Haqeeqat",
+    artist: "Javed Ali",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Aadaat",
+    artist: "Atif Aslam",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Aapki Nazro Ne Samjha",
+    artist: "Lata Mangeshkar",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Khaamoshiyan",
+    artist: "Arijit Singh",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Apna Bana Le",
+    artist: "Arijit Singh",
+    img: "music-11",
+    src: "music-11"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
